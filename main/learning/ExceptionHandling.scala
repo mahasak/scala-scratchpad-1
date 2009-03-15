735800bd3178a2910782c9261ceb86acb223e6a4
tony <tony@count-zero>
2009-03-15 08:44:37 +0000

    savepoint

---
+++ main/learning/ExceptionHandling.scala
@@ -0,0 +1,20 @@
+package learning
+
+import java.io.FileReader
+
+object ExceptionHandling  {
+  def main(args: Array[String]): Unit = {
+    // Simple case, note the assignment
+    val isOpened: Boolean = 
+	    try {
+	      new FileReader("made-up-name")
+          true
+	    } 
+	    catch {
+	      case ex: java.io.FileNotFoundException =>
+	        false
+	    }
+    assert (isOpened == false)
+  }
+  
+}
