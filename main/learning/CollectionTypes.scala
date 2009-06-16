89803758a0a33899b5cf5fc8afcca48327c2a72e
Tony <tony@neuromancer>
2009-06-16 20:12:53 +0100

    savepoint

--- main/learning/CollectionTypes.scala
+++ main/learning/CollectionTypes.scala
@@ -3,6 +3,5 @@ package learning;
 object CollectionTypes {
 
   def main(args: Array[String]): Unit = {
-    
   }
 }
--- main/learning/ExceptionHandling.scala
+++ main/learning/ExceptionHandling.scala
@@ -3,18 +3,18 @@ package learning
 import java.io.FileReader
 
 object ExceptionHandling  {
-  def main(args: Array[String]): Unit = {
-    // Simple case, note the assignment
-    val isOpened: Boolean = 
-	    try {
-	      new FileReader("made-up-name")
-          true
-	    } 
-	    catch {
-	      case ex: java.io.FileNotFoundException =>
+    def main(args: Array[String]): Unit = {
+        // Simple case, note the assignment
+        val isOpened: Boolean =
+            try {
+                new FileReader("made-up-name")
+                true
+            }
+            catch {
+                case ex: java.io.FileNotFoundException =>
 	        false
-	    }
-    assert (isOpened == false)
-  }
+            }
+        assert (isOpened == false)
+    }
   
 }
