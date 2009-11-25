2bf270dc8ec7c19ce5fcac1fd4adb4f46caf3592
tony <tony@count-zero>
2009-11-25 16:54:42 +0000

    savepoint

--- src/test/scala/learning/LoopsAndControlSpec.scala
+++ src/test/scala/learning/LoopsAndControlSpec.scala
@@ -8,10 +8,32 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class LoopsAndControlSpec extends Spec with ShouldMatchers {
-     describe("") {
-      //Setup
-      it("") {
-        pending
+    describe("A simple 'for' loop") {
+      var result = ""
+      for (val i <- 1 to 9)
+          result += i
+      it("should execute the body with each iteration") {
+        result should be ("123456789")
+      }
+    }
+    describe("A 'for' loop with a filter clause") {
+      var result = ""
+      for (val i <- 1 to 10; (i%3 == 0))
+          result += i
+      it("should only execute the body when the filter condition is met") {
+        result should be ("369")
+      }
+    }
+    describe("A 'for' loop with a filter clause which does something, but returns a boolean") {
+      var result = ""
+      def doStuff(i: Int): Boolean = {
+        // Do anything
+        true
+      }
+      for (val i <- 1 to 9; doStuff(i))
+          result += i
+      it("should only execute the body when the filter condition is met") {
+        result should be ("123456789")
       }
     }
 }
