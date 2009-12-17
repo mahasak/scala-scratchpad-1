69e55ba00fd153367388b7a1dc0af4180da4c387
tony <tony@count-zero>
2009-12-17 17:10:17 +0000

    Kata 9: Acceptance tests passing

--- src/main/scala/kata9/CheckOut.scala
+++ src/main/scala/kata9/CheckOut.scala
@@ -4,14 +4,26 @@ class CheckOut {
     var rules: List[(List[String] => Int)] = Nil
     var items = List[String]()
 
-    def countItems(item: String, items: List[String]) = items.filter(i => i == item).length
-    def costItems(item: String, items: List[String], cost: Int) = countItems(item, items) * cost
+    private def countItems(item: String, items: List[String]) = items.filter(i => i == item).length
+    private def parseDeal(deal: String) = {
+        val components = deal.split(" for ").map(_.toInt)
+        (components(0), components(1))
+    }
+    private def costItems(item: String, items: List[String], basicCost: Int, deal: String) = {
+        val (discountQty, discountPrice) = if (deal equals "" ) (1, basicCost) else parseDeal(deal)
+        val numItems = countItems(item, items)
+        val numBundles: Int = numItems / discountQty
+        val numBasic = numItems % discountQty
+
+        (numBasic * basicCost)+(numBundles * discountPrice)
+    }
 
     def this(ruleDefs: Map[String, (Int, String)]) = {
         this()
         ruleDefs.keysIterator.foreach { item =>
           val cost = ruleDefs(item)._1
-          val costOfItems = costItems(item, _: List[String], cost)
+          val deal = ruleDefs(item)._2
+          val costOfItems = costItems(item, _: List[String], cost, deal)
           rules = costOfItems :: rules
         }
     }
--- src/test/scala/kata9/CheckOutSpec.scala
+++ src/test/scala/kata9/CheckOutSpec.scala
@@ -10,7 +10,8 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class CheckOutSpec extends Spec with ShouldMatchers {
     val RULES = Map(
-        "A" -> (50, "")
+        "A" -> (50, ""),
+        "B" -> (10, "2 for 15")
     )
     describe("A supermarket check-out") {
       val co = new CheckOut(RULES)
@@ -23,5 +24,17 @@ class CheckOutSpec extends Spec with ShouldMatchers {
         co.scan("A")
         co.total() should be (100)
       }
+    }
+    describe("A supermarket check-out applying discounts") {
+        val co = new CheckOut(RULES)
+        it("should begin a transaction with zero cost") {
+            co.total() should be (0)
+        }
+        it("should add the cost of each item scanned") {
+            co.scan("B")
+            co.total() should be (10)
+            co.scan("B")
+            co.total() should be (15)
+        }
     }
 }
