4a417ff98d730c1f308ace213a0653f1cb72e1a5
tony <tony@count-zero>
2010-01-31 20:40:53 +0000

    savepoint

--- src/main/scala/calc/StringCalculator.scala
+++ src/main/scala/calc/StringCalculator.scala
@@ -1,13 +1,24 @@
 package calc
 
 
-
 class StringCalculator {
-  def add(numbers: String): Int = {
-      implicit def toIntOverload(s: String) = new {
-        def safeToInt: Int = if (s.isEmpty) 0 else s.toInt
-      }
+  val DelimRE = """//(.+)\n.*""".r
   
-      (0 /: numbers.split("""[,\n]+"""))((t,n) => t + n.safeToInt)
+  def add(numbers: String): Int = {
+    implicit def toIntOverload(s: String) = new {
+      def safeToInt: Int = if (s.matches("""^\d+$""")) s.toInt else 0
     }
+    
+    def extractPrefix(s: String): String = {
+        val DelimRE(prefix) = s
+        prefix
+    }
+
+    val extraPrefix = DelimRE.findPrefixOf(numbers) match { 
+      case s: Some[String] => extractPrefix(numbers)
+      case _ => ""     
+    }  
+
+    (0 /: numbers.split("""[,\n\\$extraPrefix]+"""))((t, n) => t + n.safeToInt)
+  }
 }
--- src/test/scala/calc/StringCalculatorSpec.scala
+++ src/test/scala/calc/StringCalculatorSpec.scala
@@ -29,5 +29,9 @@ class StringCalculatorSpec extends Spec with ShouldMatchers {
     describe("when given a string separated by a mixture of newlines and commas") {
       it("should return 13 for a string with 1,2 & 10 separated by a newline and a comma") { calc.add("1\n10,2") should be (13) }
     }
+
+    describe("when given a string specifying a new default delimiter on the first line") {
+      it("should return 11 when ';' is specified and 1 & 10 are separated by ';'") { calc.add("//;\n1;10") should be (11) }
+    }
   }
 }
