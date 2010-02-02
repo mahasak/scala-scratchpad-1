95d506cafce21772c65cbe816c632cc7c81041d6
Tony <bainesap@btp003628>
2010-02-02 13:53:15 +0000

    savepoint

--- src/main/scala/calc/StringCalculator.scala
+++ src/main/scala/calc/StringCalculator.scala
@@ -2,26 +2,25 @@ package calc
 
 
 class StringCalculator {
-   val DelimMatchRE = """(?s)//.{1}\n.*""".r
-   val DelimExtractRE = """(?s)//(.{1})\n.*""".r
+   val DelimRE = """(?s)//(.)\n.*""".r
 
   
    def add(numbers: String): Int = {
-    implicit def toIntOverload(s: String) = new {
-      def safeToInt: Int = if (s.matches("""^\d+$""")) s.toInt else 0
-    }
+    def safeToInt(s: String): Int = if (s.matches("""^\d+$""")) s.toInt else 0
     
-    def extractPrefix(s: String): String = {
-        val DelimExtractRE(prefix) = s
+    def getDelim(s: String): String = {
+        val DelimRE(prefix) = s
         prefix
     }
 
-    val extraPrefix = DelimMatchRE findPrefixOf numbers match { 
-        case Some(s) => extractPrefix(numbers)
+    val extraPrefix = DelimRE findPrefixOf numbers match { 
+        case Some(s) => getDelim(numbers)
         case None => ""
     }  
     
+    val splitExpression = """[,\n"""+ extraPrefix +"""]+"""
+    
     // Fold-left operator
-    (0 /: numbers.split("""[,\n"""+ extraPrefix +"""]+"""))((t, n) => t + n.safeToInt)
+    (0 /: numbers.split(splitExpression))((t, n) => t + safeToInt(n))
   }
 }
--- src/test/scala/calc/StringCalculatorSpec.scala
+++ src/test/scala/calc/StringCalculatorSpec.scala
@@ -14,27 +14,27 @@ class StringCalculatorSpec extends Spec with ShouldMatchers {
 
     describe("when given a string separated by commas") {
       it("should return zero for an empty string") { calc.add("") should be (0) }
-      it("should return 1 for a string with just 1") { calc.add("1") should be (1) }
-      it("should return 10 for a string with just 10") { calc.add("10") should be (10) }
-      it("should return 11 for a string with 1 & 10") { calc.add("1,10") should be (11) }
-      it("should return 1 for a string with 1 followed by a comma") { calc.add("1,") should be (1) }
-      it("should return 1 for a string with 1 preceeded by a comma") { calc.add(",1") should be (1) }
+      it("should add a single-digit number") { calc.add("1") should be (1) }
+      it("should add a double-digit number") { calc.add("10") should be (10) }
+      it("should add numbers seperated by commas") { calc.add("1,10") should be (11) }
+      it("should still work if there is a trailing commas") { calc.add("1,") should be (1) }
+      it("should still work if there is a leading commas") { calc.add(",1") should be (1) }
       it("should return zero for a string with just a comma") { calc.add(",") should be (0) }
     }
 
     describe("when given a string separated by newlines") {
-      it("should return 11 for a string with 1 & 10 separated by a newline") { calc.add("1\n10") should be (11) }
+      it("should add numbers seperated by a newline") { calc.add("1\n10") should be (11) }
     }
 
     describe("when given a string separated by a mixture of newlines and commas") {
-      it("should return 13 for a string with 1,2 & 10 separated by a newline and a comma") { calc.add("1\n10,2") should be (13) }
+      it("should add numbers seperated by a newline and a comma") { calc.add("1\n10,2") should be (13) }
     }
 
     describe("when given a string specifying a new default delimiter on the first line") {
-      it("should return 11 when ';' is specified and 1 & 10 are separated by ','") { calc.add("//;\n1,10") should be (11) }
-      it("should return 11 when ';' is specified and 1 & 10 are separated by '\\n'") { calc.add("//;\n1\n10") should be (11) }
-      it("should return 11 when ';' is specified and 1 & 10 are separated by ';'") { calc.add("//;\n1;10") should be (11) }
-      it("should return 11 when '+' is specified and 1 & 10 are separated by '+'") { calc.add("//+\n1+10") should be (11) }
+      it("should work when ';' is specified and numbers are separated by ','") { calc.add("//;\n1,10") should be (11) }
+      it("should work when ';' is specified and numbers are separated by '\\n'") { calc.add("//;\n1\n10") should be (11) }
+      it("should work when ';' is specified and numbers are separated by ';'") { calc.add("//;\n1;10") should be (11) }
+      it("should work when '+' is specified and numbers are separated by '+'") { calc.add("//+\n1+10") should be (11) }
     }
   }
 }
