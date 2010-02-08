1758c62cca9474b0a0e9dc4ffa4e47f45bfac5f4
tony <tony@count-zero>
2010-02-08 20:35:33 +0000

    savepoint

--- src/main/scala/calc/StringCalculator.scala
+++ src/main/scala/calc/StringCalculator.scala
@@ -7,20 +7,20 @@ class StringCalculator {
 
    private def safeToInt(s: String): Int = if (s.matches("""^\-{0,1}\d+$""")) s.toInt else 0
    private def filterOutOfRange(i: Int): Int = if (i < MaxIntValue) i else 0
-   
+
    def add(input: String): Int = {
     def getDelim(s: String): String = {
         val DelimRE(prefix) = s
         prefix
     }
-    
-    val extraPrefix = DelimRE findPrefixOf input match { 
+
+    val extraPrefix = DelimRE findPrefixOf input match {
         case Some(s) => getDelim(input)
         case None => ""
-    }  
-    
+    }
+
     val splitExpression = """[,\n"""+ extraPrefix +"""]+"""
-    
+
     val numbers = input.split(splitExpression).map(i => filterOutOfRange(safeToInt(i)))
     val negativeNumbers = numbers.filter(_<0)
     if (negativeNumbers.size > 0) throw new IllegalArgumentException("negatives not allowed: ["+ negativeNumbers.mkString(",") + "]")
