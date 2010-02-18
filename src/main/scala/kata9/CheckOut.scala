c7a7478a166d0b321142a16a8fcdf56910e526d6
Tony <tony@neuromancer>
2010-02-18 11:22:30 +0000

    More tidy up

--- src/main/scala/kata9/CheckOut.scala
+++ src/main/scala/kata9/CheckOut.scala
@@ -1,22 +1,12 @@
 package kata9
 
-class CheckOut {
-    var rules: List[(List[String] => Int)] = Nil
+class CheckOut(ruleDefs: Map[String, (Int, String)]) {
+    val rules = buildRules(ruleDefs)
     var items = List[String]()
 
-    private def countItems(item: String, items: List[String]) = items.filter(i => i == item).length
-    private def parseDeal(deal: String) = {
-        val components = deal.split(" for ").map(_.toInt)
-        (components(0), components(1))
-    }
-    private def costItems(item: String, items: List[String], basicCost: Int, deal: String) = {
-        val (discountQty, discountPrice) = if (deal equals "" ) (1, basicCost) else parseDeal(deal)
-        val numItems = countItems(item, items)
-        val numBundles: Int = numItems / discountQty
-        val numBasic = numItems % discountQty
+    def scan(item: String) = items = item :: items
+    def total(): Int = (0 /: rules.map( costOfItems => costOfItems(items) ))(_+_)
 
-        (numBasic * basicCost)+(numBundles * discountPrice)
-    }
     private def buildRules(ruleDefs: Map[String, (Int, String)]) = {
         ruleDefs.keys.map { item =>
           val cost = ruleDefs(item)._1
@@ -25,16 +15,19 @@ class CheckOut {
         }.toList
     }
 
-    def this(ruleDefs: Map[String, (Int, String)]) = {
-        this()
-        rules = buildRules(ruleDefs)
+    private def costItems(item: String, items: List[String], basicCost: Int, deal: String) = {
+        val (discountQty, discountPrice) = if (deal equals "" ) (1, basicCost) else parseDeal(deal)
+        val numItems = countItems(item, items)
+        val numBundles: Int = numItems / discountQty
+        val numBasic = numItems % discountQty
+
+        (numBasic * basicCost)+(numBundles * discountPrice)
     }
 
-    def scan(item: String) = items = item :: items
-    def total(): Int = {
-      var totalCost = 0;
-      rules.map( costOfItems => costOfItems(items) ).foreach(c => totalCost += c)
-      totalCost
+    private def parseDeal(deal: String) = {
+        val components = deal.split(" for ").map(_.toInt)
+        (components(0), components(1))
     }
 
+    private def countItems(item: String, items: List[String]) = items.filter(i => i == item).length
 }
